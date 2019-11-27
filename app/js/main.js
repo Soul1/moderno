048729bbc52aea2995824b3d4fc42889168e63bf
Soul1 <darkwaud@gmail.com>
2019-11-27 14:13:48 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,18 @@ $(function () {
   
   var mixer = mixitup('.products__inner-box');
 
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+  });
+
+  $('.product-slider__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    dots: true,
+    
+  });
 
 });
